faecc54c2dd02b3dde7167692a534d3690492f65
Syed Zawad <syedzawadd@gmail.com>
2022-11-12 22:16:52 +0600

    extra another section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoaderData, Link } from 'react-router-dom';
-import img from '../../assets/images/dentistry-right.png'
+import img from '../../assets/images/dentistry-right.png';
+import img01 from '../../assets/images/dentist-friendly.png'
 import Hero from '../../components/Hero/Hero';
 import ServiceCard from '../../components/ServiceCard/ServiceCard';
 
@@ -29,7 +30,7 @@ const Home = () => {
             <section style={{ maxWidth: '1320px' }} className='mt-24 mx-auto'>
                 <div className='hero-bg py-12 rounded-2xl'>
                     <div className="hero-content mx-auto flex-col lg:flex-row-reverse lg:justify-between px-14">
-                        <img src={img} alt='dentist' className="max-w-sm" />
+                        <img src={img} alt='dentist' className="max-w-sm  animate-bounce" />
                         <div className='flex flex-col items-start'>
                             <h1 style={{ color: '#0F3E3E' }} className='mt-2 text-left text-5xl font-semibold'>Dentistry Done Right</h1>
                             <p style={{ color: '#3D6666' }} className="py-6 text-left">Adults and kids, I welcome patients of all ages! <br /> I am passionate about building lifetime relationships <br /> through positive experiences</p>
@@ -39,12 +40,13 @@ const Home = () => {
                 </div>
             </section>
             <section style={{ maxWidth: '1320px' }} className='mt-24 mx-auto'>
-                <div className='hero-bg py-12 rounded-2xl'>
+                <div className='py-12 rounded-2xl'>
                     <div className="hero-content mx-auto flex-col lg:flex-row lg:justify-between px-14">
-                        <img src={img} alt='dentist' className="max-w-sm" />
+                        <img src={img01} alt='dentist' className="max-w-sm" />
                         <div className='flex flex-col items-start'>
-                            <h1 style={{ color: '#0F3E3E' }} className='mt-2 text-left text-5xl font-semibold'>Dentistry Done Right</h1>
-                            <p style={{ color: '#3D6666' }} className="py-6 text-left">Adults and kids, I welcome patients of all ages! <br /> I am passionate about building lifetime relationships <br /> through positive experiences</p>
+                            <p style={{ color: '#3D6666' }} className='text-left text-sm tracking-wider'>TRANSPARENT PRICING</p>
+                            <h1 style={{ color: '#0F3E3E' }} className='mt-2 text-left text-5xl font-semibold'>No Surprises</h1>
+                            <p style={{ color: '#3D6666' }} className="py-6 text-left">I believe in accessible dental care. <br /> I offer straightforward pricing, clear estimates, <br /> and an unprecedented dental warranty. <br /> Keep up with your routine 6-month checkups <br /> and if something needs fixing, I’ll handle it at no cost to you.</p>
                             <button className="btn btn-primary capitalize text-lg px-6">Learn More</button>
                         </div>
                     </div>
