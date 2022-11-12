77011a3464cfe8cad767a020aabd0d1b7bcbf834
Syed Zawad <syedzawadd@gmail.com>
2022-11-12 20:33:41 +0600

    service sections added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,16 +9,19 @@ const Home = () => {
     return (
         <div>
             <Hero></Hero>
-            <section>
-                <h1>Dr. Fill's Services</h1>
+            <section style={{ maxWidth: '1320px', color: '#0F3E3E' }} className='mt-24 mx-auto'>
+                <h1 className='text-5xl font-semibold'>Dr. Fill's Services</h1>
 
-                <div>
+                <div className='mt-16 mb-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                     {
                         services.map(service => <ServiceCard
                             key={service._id}
                             service={service}
                         ></ServiceCard>)
                     }
+                </div>
+                <div>
+                    <button className='btn btn-primary capitalize text-lg px-6'>See all Services</button>
                 </div>
             </section>
         </div>
