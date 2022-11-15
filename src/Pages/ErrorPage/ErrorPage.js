2122c9083bad18f031fe312f5bb40750c4a5d573
Syed Zawad <syedzawadd@gmail.com>
2022-11-15 23:17:56 +0600

    last update

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import img from '../../assets/images/error.png';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
         <div>
+            <h1 className='text-5xl'>OOPS!!</h1>
+            <p>The Page You have been looking for could not found</p>
             <img src={img} alt="" />
+            <Link to='/'>
+                <button>Back to Homepage</button>
+            </Link>
         </div>
     );
 };
